519e7bba2e3343c448fe3baa9e82ba9de8a8159f
Unknown <shterion.yanev@yahoo.com>
2018-10-31 22:02:38 +0100

    Create custom validations for education, experience and post

---
+++ validation/education.js
@@ -0,0 +1,34 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+const validateEducationInput = (data) => {
+  let errors = {};
+
+  data.school = !isEmpty(data.school) ? data.school : '';
+  data.degree = !isEmpty(data.degree) ? data.degree : '';
+  data.fieldOfStudy = !isEmpty(data.fieldOfStudy) ? data.fieldOfStudy : '';
+  data.from = !isEmpty(data.from) ? data.from : '';
+
+  if (Validator.isEmpty(data.school)) {
+    errors.school = 'School field is required';
+  }
+
+  if (Validator.isEmpty(data.degree)) {
+    errors.degree = 'Degree title field is required';
+  }
+
+  if (Validator.isEmpty(data.fieldOfStudy)) {
+    errors.fieldOfStudy = 'FieldOfStudy date field is required';
+  }
+
+  if (Validator.isEmpty(data.from)) {
+    errors.from = 'From date field is required';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  }
+};
+
+module.exports = validateEducationInput;
