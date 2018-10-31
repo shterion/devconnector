6e2027db68659f1b177d53e0b1031e8cef487f2e
Unknown <shterion.yanev@yahoo.com>
2018-10-31 17:32:58 +0100

    Create posts route for creating a new post

--- routes/api/controllers/posts.js
+++ routes/api/controllers/posts.js
@@ -1,11 +1,40 @@
+/* eslint consistent-return: "off" */
 const express = require('express');
-
+const passport = require('passport');
 const router = express.Router();
+const log = require('./../utils/logger');
 
-router.get('/', (req, res) => {
-  res.json({
-    msg: 'Posts',
-  });
+// Load validation
+const validatePostInput = require('./../../../validation/post');
+
+//Load models
+const Post = require('./../../../models/Post');
+
+// @route   POST api/posts
+// @desc    Create post
+// @access  Private
+router.post('/', passport.authenticate('jwt', {
+  session: false
+}), async (req, res) => {
+
+  const { errors, isValid } = validatePostInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  const body = {
+    text: req.body.text,
+    name: req.body.name,
+    avatar: req.body.avatar,
+    user: req.user.id,
+  };
+
+  try {
+    const newPost = await new Post(body).save();
+    res.json(newPost);
+  } catch (e) {
+    log.error(e);
+  }
 });
 
 module.exports = router;
