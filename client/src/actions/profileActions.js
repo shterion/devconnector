df6ac7639246a89234703ac5bf0c8ee398ac84fc
Unknown <shterion.yanev@yahoo.com>
2018-11-18 20:12:46 +0100

    Add action for create profile

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE } from './types';
+import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS } from './types';
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -24,6 +24,18 @@ export const setProfileLoading = () => {
         type: PROFILE_LOADING
     };
 }
+
+// Create Profile
+export const createProfile = (profileData, history) => dispatch => {
+    axios.post('/api/profile', profileData)
+        .then(res => history.push('/dashboard'))
+        .catch(err => 
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+};
 
 // Clear profile
 export const clearCurrentProfile = () => {
