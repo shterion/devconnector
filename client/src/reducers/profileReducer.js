53af27930073a2ae608a1c2ef4c3b7cb061e7b04
Unknown <shterion.yanev@yahoo.com>
2018-12-06 21:55:33 +0100

    Add get profiles action and reducer

--- client/src/reducers/profileReducer.js
+++ client/src/reducers/profileReducer.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, CREATE_PROFILE } from '../actions/types';
+import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, CREATE_PROFILE, GET_PROFILES } from '../actions/types';
 
 const initialState = {
     profile: null,
@@ -23,6 +23,12 @@ export default function (state = initialState, action) {
             return {
                 ...state,
                 profile: action.payload
+            }
+        case GET_PROFILES:
+            return {
+                ...state,
+                profiles: action.payload,
+                loading: false
             }
         case CLEAR_CURRENT_PROFILE: 
             return {
