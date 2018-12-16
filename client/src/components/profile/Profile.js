785d3f577916d3bf2fc8b6a54d83d453bcbfeec3
Unknown <shterion.yanev@yahoo.com>
2018-12-16 20:50:06 +0100

    Add more functionality to profile component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -16,6 +16,12 @@ class Profile extends Component {
         }
     }
 
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.profile.profile === null && this.props.profile.loading) {
+            this.props.history.push('/not-found');
+        }
+    }
+
     render() {
         const { profile, loading } = this.props.profile;
         let profileContent;
@@ -31,10 +37,10 @@ class Profile extends Component {
                         </div>
                         <div className="col-md-6"></div>
                     </div>
-                    <ProfileHeader profile={profile}/>
-                    <ProfileAbout />
-                    <ProfileCreds />
-                    <ProfileGithub />
+                    <ProfileHeader profile={profile} />
+                    <ProfileAbout profile={profile} />
+                    <ProfileCreds education={profile.education} experience={profile.experience} />
+                    {profile.githubusername ? (<ProfileGithub username={profile.githubusername} />) : null}
                 </div>
             );
         }
