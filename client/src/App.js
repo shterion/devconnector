b101803735ed1097847dbdd3fa4a7ba57c02142c
Shterion Yanev <shterion.yanev@yahoo.com>
2019-01-12 23:19:52 +0100

    Add a post feed in navbar

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,7 @@ import AddExperience from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 import NotFound from './components/not-found/NotFound';
 
 import './App.css';
@@ -74,6 +75,9 @@ class App extends Component {
               </Switch>
               <Switch>
                 <PrivateRoute exact path="/add-education" component={AddEducation}></PrivateRoute>
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts}></PrivateRoute>
               </Switch>
               <Route exact path="/not-found" component={NotFound} />
             </div>
